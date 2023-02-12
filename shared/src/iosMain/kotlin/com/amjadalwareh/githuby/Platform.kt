c12eae12f3775635c8b76d98e000bebfae05c858
Amjad <a.alwareh25@gmail.com>
2023-02-12 14:07:43 +0100

    first commit

---
+++ shared/src/iosMain/kotlin/com/amjadalwareh/githuby/Platform.kt
@@ -0,0 +1,9 @@
+package com.amjadalwareh.githuby
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
